2c4ee28b13e2ef24bf02a97135b5d7593b06d1cb
Sergey Romanov <ssromanov@gmail.com>
2014-10-23 07:18:27 +0400

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -60,6 +60,8 @@ gulp.task('fonts', function () {
 gulp.task('extras', function () {
     return gulp.src(['app/*.*', '!app/*.html'], { dot: true })
         .pipe(gulp.dest('dist'));
+    return gulp.src(['app/*', '!app/*.html'], { dot: true })
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', function () {
